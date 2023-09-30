a5fe69843c25773dda6a0e4825a0f273d832a940
Bentebent <henriksson.tim@gmail.com>
2023-09-30 10:02:31 +0200

    add first person controller

---
+++ src/camera_controller.gd
@@ -0,0 +1,6 @@
+extends Camera3D
+class_name CameraController
+
+func rotate_me(mouse_movement: Vector2, sensitivity: float):
+    rotate_x(-mouse_movement.y * sensitivity)
+    rotation.x = clamp(rotation.x, -1.2, 1.2)
---
+++ src/player_controller.gd
@@ -0,0 +1,46 @@
+extends CharacterBody3D
+
+@export var speed = 0
+@export var sensitivity = 0.02
+
+@onready var camera : CameraController = $"Camera/Camera3D"
+
+var target_velocity = Vector3.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		camera.rotate_me(event.relative, sensitivity)
+		rotate_y(-event.relative.x * sensitivity)
+
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+
+	if Input.is_action_pressed("move_right"):
+		direction += global_transform.basis.x
+	if Input.is_action_pressed("move_left"):
+		direction -= global_transform.basis.x
+	if Input.is_action_pressed("move_bwd"):
+		direction += global_transform.basis.z
+	if Input.is_action_pressed("move_fwd"):
+		direction -= global_transform.basis.z
+
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+	
+	target_velocity.x = direction.x  * speed
+	target_velocity.z = direction.z *  speed
+
+	velocity = target_velocity
+	move_and_slide()
+
+
+		
